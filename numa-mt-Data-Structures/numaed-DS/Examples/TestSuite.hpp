48ecc8bb202eb30a24b40af4de2b2a26ddb7505d
machtvonjoseph <kiwo9430@colorado.edu>
2024-10-14 12:18:06 -0600

    numathreads library added. num-mt-Data-Structures use numathreads. It has Debug options

---
+++ numa-mt-Data-Structures/numaed-DS/Examples/TestSuite.hpp
@@ -0,0 +1,24 @@
+/*! \file TestSuite.hpp
+ * \brief Testsuite program which allows for testing of various data structures
+ * \author Nii Mante
+ * \date 10/28/2012
+ *
+ */
+
+#ifndef _TESTSUITE_HPP_
+#define _TESTSUITE_HPP_
+
+
+/*!
+ * \brief Test function for the Stack classes
+ * 
+ * Invokes the functions of the Stack class 
+ * (pop, push, ctor, dtor, display)
+ * 
+ */
+void StackTest();
+void BinarySearchTest();
+
+
+
+#endif //_TESTSUITE_HPP_
